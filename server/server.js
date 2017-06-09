0c6a1beebfc8592a6c157a44cfe1adc5620c9a3f
satish <satish_cards@yahoo.com>
2017-06-09 17:18:59 +0530

    create and generate msg utility

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express=require('express');
 const http=require('http');
 const socketIO=require('socket.io');
 var app=express();
-
+const {generateMessage}=require('./utils/message');
 var publicPath=path.join(__dirname,"../public");
 app.use(express.static(publicPath));
 const port=process.env.PORT || 3000;
@@ -16,13 +16,21 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log("Client Disconnected");
     })
+    socket.emit('newMessage',generateMessage('Admin','Welcome To ChatMessage'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined'));
     socket.on('createMessage',(message)=>{
-        io.emit('newMessage',message);
-        console.log(message);
+        io.emit('newMessage',generateMessage(message.from,message.text)); 
     });
+        // socket.broadcast.emit('newMessage',{
+        //     from:message.from,
+        //     text:message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        //console.log(message);
+    //});
     
 
-})
+});
 server.listen(port,()=>{
     console.log(`server listening on port ${port}`);
 });
