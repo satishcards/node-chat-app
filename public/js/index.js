5134c1d8b2f5a9e4a765da6b70ce621372973846
satish <satish_cards@yahoo.com>
2017-06-10 11:16:36 +0530

    addind CSS

--- public/js/index.js
+++ public/js/index.js
@@ -23,26 +23,31 @@ socket.on('newLocationMessage',function(message){
 });
 var locationButton=jQuery('#send-location');
 locationButton.on('click',function(){
+    
     if(!navigator.geolocation){
         return alert('Geo Location not supported by your browser');
     }
+    locationButton.attr('disabled','disabled').text('Sending Location');
     navigator.geolocation.getCurrentPosition(function(position){
         //console.log(position);
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         });
     },function(){
+        locationButton.removeAttr('disabled').text('Send Location');
         return alert('Unable to fetch Geo-Location');
     });
 });
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
+    var textBoxMessage=jQuery('[name=message]');
    // console.log("hi");
     socket.emit('createMessage',{
         from:'satish',
-        text:jQuery('[name=message]').val()
+        text:textBoxMessage.val()
     },function(){
-
+        textBoxMessage.val("");
     });
 });
