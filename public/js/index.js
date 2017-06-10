ae0ff1bad826294696833ec7a574ec48d461cd8b
satish <satish_cards@yahoo.com>
2017-06-10 15:39:27 +0530

    scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,22 @@
  //var moment =require('moment');
  //var moment=require('moment');
  var socket=io();
+function scrollToBottom(){
 
+    var messages=jQuery('#messages');
+    var newMessage=messages.children('li:last-child');
+
+    var clientHeight=messages.prop('clientHeight');
+    var scrollTop=messages.prop('scrollTop');
+    var scrollHeight=messages.prop('scrollHeight');
+    var newMessageHeight=newMessage.innerHeight();
+    var lastMessageHeight=newMessage.prev().innerHeight();
+    if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight)
+    {
+        messages.scrollTop(scrollHeight);
+    }
+
+};
 socket.on('connect',function(){
     console.log('Connected to Server');
     socket.emit('newEmail',{to:'satish',text:'Hey'});
@@ -18,7 +33,7 @@ socket.on('newMessage',function(message){
         createdAt:formattedTime
     });
     jQuery('#messages').append(html);
-    
+    scrollToBottom();
 });
 socket.on('newLocationMessage',function(message){
     var formattedTime=moment(message.createdAt).format('h:mm a');
@@ -34,6 +49,7 @@ socket.on('newLocationMessage',function(message){
     // a.attr('href',message.url);
     // li.append(a);
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 var locationButton=jQuery('#send-location');
 locationButton.on('click',function(){
